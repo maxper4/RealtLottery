2dedc7eaddcae41362ae70941495759083426f53
maxper4 <maxenceperion@gmail.com>
2024-01-16 01:20:17 +0100

    Better (debug) deployment script & example of env

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 coverage:; forge coverage --report lcov && genhtml lcov.info --branch-coverage --output-dir coverage
 
 script:;
-	forge script ${script} --rpc-url ${GNOSIS_TESTNET_URL} --private-key ${PRIVATE_KEY} --broadcast 
+	forge script ${script} --rpc-url ${GNOSIS_TESTNET_URL} --private-key ${PRIVATE_KEY} --broadcast --verify
 
 verif:
-	forge verify-contract ${address} ${name} --verifier-url https://gnosis-chiado.blockscout.com/api --watch 
+	forge verify-contract ${address} ${name} --verifier-url https://gnosis-chiado.blockscout.com/api --watch
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "../src/RealtLottery.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract Deploy is Script {
     function setUp() public {}
 
     function run() public {
         address[] memory tokens = new address[](1);
-        tokens[0] = address(0x0);
         uint256[] memory interests = new uint256[](1);
-        interests[0] = 1000000000000000000;
+        interests[0] = 1000;
         address[] memory rentTokens = new address[](1);
         rentTokens[0] = 0x063Fbb248945656898938137A7A048bdCe847327;
         uint256 nextDraw = block.timestamp;         // For debug
@@ -19,6 +19,10 @@ contract Deploy is Script {
         address witnet = 0x0123456fbBC59E181D76B6Fe8771953d1953B51a;        // For gnosis testnet
 
         vm.startBroadcast();
+        address token = address(new ERC20Mock("Test Token", "TST", 0xfdf3403d3426C6ecC7C2acb9cdE70ca369445836, 100));     // For debug
+        address tokenRent = address(new ERC20Mock("Test Rent", "TRR", 0xfdf3403d3426C6ecC7C2acb9cdE70ca369445836, 100));     // For debug
+        tokens[0] = token;
+        rentTokens[0] = tokenRent;
         RealtLottery lottery = new RealtLottery(tokens, interests, rentTokens, nextDraw, witnet);
         vm.stopBroadcast();
     }
